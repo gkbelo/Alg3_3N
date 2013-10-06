b44df6b2ef14e37046e504062307e7f7e9441b16
gkbelo <gkbelo@gmail.com>
2013-10-06 14:35:20 -0300

    Release 2

--- Trabalho_2/src/Imagem.java
+++ Trabalho_2/src/Imagem.java
@@ -4,40 +4,64 @@ import java.io.IOException;
 
 public class Imagem {
 
-	private int[][] img;
+	private int[][][] img;
 	private String tpCor = "";
+	private int Alt;
+	private int Lar;
 	
-	public Imagem(int alt, int lar, String tpCor) {
-		this.img = new int[alt][lar];
+	public Imagem(int pAlt, int pLar, String tpCor) {
+		this.Alt = pAlt;
+		this.Lar = pLar;
+		this.img = new int[Alt][Lar][3];
 		this.tpCor = tpCor;
 	}
 	
 	public String getTpCor() {
 		return tpCor;
 	}
+
+	public int getAltura() {
+		return Alt;
+	}
+
+	public int getLargura() {
+		return Lar;
+	}
+
 	
 	public void Salvar( String filename ) throws IOException {
 		BufferedWriter out = new BufferedWriter( new FileWriter(filename + ".pnm") );
 
 		if ( this.tpCor.equalsIgnoreCase("G") ){
-			out.write(String.format("P2\n%d %d\n255\n", img[0].length, img[0].length ));
-		} /* else {
-			out.write(String.format("P3\n%d %d\n255\n", img[0].length, img[0].length ));
-		} */
-		for (int[] linha : img) {
-			for (int v : linha){
-				out.write(String.format("%d ", v) );
+			out.write("P2\n");
+		} else {
+			out.write("P3\n");
+		}
+		out.write(Alt + " " + Lar + "\n");
+		out.write("255\n");
+		
+		for(int x = 0 ; x < Alt ; x++){	
+			for(int y = 0 ; y < Lar ; y++){	
+				if( this.tpCor.equalsIgnoreCase("G") ){
+					out.write(img[x][y][0] + " ");
+				} else {
+					out.write(img[x][y][0] + " " + img[x][y][1] + " " + img[x][y][2] + " " );
+				}
 			}
-		}	
+		}
 		out.close();
 	}
 
 	public void SetPixel( Ponto p, Cor c ){
  
 		if ( this.tpCor.equalsIgnoreCase("G") ){
-			img[p.x][p.y] = c.getGrey();
-		} /* else {
-			img[p.x][p.y] = c.getR() c.getG() c.getB(); 
-		} */		
+			img[p.x][p.y][0] = c.getGrey();
+			img[p.x][p.y][1] = c.getGrey();
+			img[p.x][p.y][2] = c.getGrey();
+		}  else {
+			img[p.x][p.y][0] = c.getR();
+			img[p.x][p.y][1] = c.getG(); 
+			img[p.x][p.y][2] = c.getB(); 
+		} 		
 	}	
 }
